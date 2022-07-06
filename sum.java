d87979bbdd9bfe1b7df7c7c92fc69184a79b193f
NishaSinha1418 <nishaisha1899gmail.com>
2022-07-06 12:05:49 +0530

    sum java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public clas Sum{
     public static void main(String[] args){
         int a=8;
         int b=6;
-        System.out.println(a+b);
+        System.out.println("sum of two no"+ (a+b));
     }
 }
